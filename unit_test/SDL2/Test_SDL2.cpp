e3c42da9fb8c83b2a672db4eea313990e4aa3134
Nitish <ngshingde@gmail.com>
2020-05-12 22:26:13 +0530

    Implement unit testing with Catch2
    - remove the `catch.hpp` header only file
    - remove add_definitions in root CMakeLists.txt

--- unit_test/SDL2/Test_SDL2.cpp
+++ unit_test/SDL2/Test_SDL2.cpp
@@ -3,7 +3,7 @@
 //
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 #include "SDL.h"
 #include "SDL_image.h"
 #include "SDL_ttf.h"
