8a94942a53ee1bb8d53c73d328305f473748195a
Nitish Shingde <ngshingde@gmail.com>
2020-10-24 22:15:48 +0530

    REFATOR| Cosmetic changes to ViewFactory

--- src/entity/ViewFactory.cpp
+++ src/entity/ViewFactory.cpp
@@ -29,17 +29,17 @@ yaui::entity::Entity yaui::entity::ViewFactory::produceLabel(
 }
 
 yaui::entity::Entity yaui::entity::ViewFactory::produceButton(
-    yaui::entity::Registry &registry,
-    const yaui::String &textString,
-    const yaui::String &fontName,
-    const yaui::uint32 &fontSize,
-    const yaui::Colour &foregroundColour,
-    const yaui::Colour &backgroundColour,
-    const yaui::Rect &padding,
-    const yaui::Rect &border,
-    const yaui::Colour &borderColour,
-    const yaui::int32 &x,
-    const yaui::int32 &y
+    Registry &registry,
+    const String &textString,
+    const String &fontName,
+    const uint32 &fontSize,
+    const Colour &foregroundColour,
+    const Colour &backgroundColour,
+    const Rect &padding,
+    const Rect &border,
+    const Colour &borderColour,
+    const int32 &x,
+    const int32 &y
 ) {
     auto entity = produceLabel(registry, textString, fontName, fontSize, foregroundColour, backgroundColour, padding, border, borderColour, x, y);
     registry.emplace<component::MouseEventListener>(entity);
